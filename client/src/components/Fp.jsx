abaf5c57a571870dbc533135f1e279db0e8c1db4
Mitansh Patel <mitanshpatel288@gmail.com>
2023-08-24 00:44:15 +0530

    integrated and ui

--- client/src/components/Fp.jsx
+++ client/src/components/Fp.jsx
@@ -5,14 +5,16 @@ import img from '../img/boy.png'
 
 function Fp() {
     return (
-        <div className={fpc.bdy}>
-            <div className={fpc.heading}>
-                <h1 className={fpc.h}>New to the Crypto World?</h1>
-                <h2 style={{fontFamily:"var(--font2)", fontSize:"1.2rem"}}>Your Trustworthy Partner in Crypto Investment!</h2>
-                <div className={fpc.btn} > <Link  to='/chat'>Chat now</Link></div>
+        <>
+            <div className={fpc.bdy}>
+                <div className={fpc.heading}>
+                    <h1 className={fpc.h}>New to the <span className='text-yellow-400'>Crypto</span> World?</h1>
+                    <h2 style={{fontFamily:"var(--font2)", fontSize:"1.2rem"}}>Your Trustworthy Partner in Crypto Investment!</h2>
+                    <div className={fpc.btn} > <Link  to='/chat'>Chat now</Link></div>
+                </div>
+                <div className={fpc.img}><img src={img} alt='err' ></img></div>
             </div>
-            <div className={fpc.img}><img src={img} alt='err' ></img></div>
-        </div>
+        </>
     )
 }
 
--- client/src/screen/Chat.jsx
+++ client/src/screen/Chat.jsx
@@ -3,30 +3,30 @@ import css from '../astylus/chat.module.css'
 
 function Chat() {
     const [input, setInput] = useState("");
-    const[arr] = useState([]);
-    const mssgend=useRef();
+    const [arr] = useState([]);
+    const mssgend = useRef();
 
-    useEffect(()=>{               //to automatically scroll down after an input is sent
-        mssgend.current?.scrollIntoView({behavior: "smooth"})
+    useEffect(() => {               //to automatically scroll down after an input is sent
+        mssgend.current?.scrollIntoView({ behavior: "smooth" })
     }, [input])
 
     function changeInput(e) {
         setInput(e.target.value);
     }
 
-    const send= async (e)=> {
+    const send = async (e) => {
         e.preventDefault();
-        if(input !==""){
+        if (input !== "") {
             arr.push(input);
-            const res=await fetch("http://localhost:5000/chat", {
-                method: "POST",
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({mssg: input})
-            });
-            const json= await res.json();
-            console.log(json);
+            // const res=await fetch("http://localhost:5000/chat", {
+            //     method: "POST",
+            //     headers:{
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify({message: input})
+            // });
+            // const json= await res.json();
+            // const recv = json.answer;
             setInput("");
         }
     }
@@ -38,22 +38,32 @@ function Chat() {
                 <h1><i class="fa-solid fa-robot fa-shake fa-lg"></i><span className='ml-2'>Krypto Bot</span></h1>
             </div>
 
-            <div className={css.chatArea}  >
-            {
-                arr.map((data, i) => {
-                    return (
-                        <div key={i} className={css.mssg}>
-                            <p>{data}</p>
-                        </div>
-                    )
-                })
-            }
-            <div ref={mssgend}></div>
+            <div className={css.sidebar}>
+                <h1 className="mb-12 text-xl">Hello, Sam!</h1>
+                    <button className={css.btn}>FAQ</button>
+                    <button className={css.btn}>Analytics</button>
+                    <button className={css.btn}>Price</button>
+                
             </div>
 
-            <div >
+            <div className={css.chatArea}  >
+                {
+                    arr.map((data, i) => {
+                        return (
+                            <div key={i} className={css.mssg}>
+                                <p>{data}</p>
+                            </div>
+                        )
+                    })
+                }
+
+                {/* for autoscroll */}
+                <div ref={mssgend}></div>
+            </div>
+
+            <div className={css.foot}>
                 <form onSubmit={send} className={css.inp}>
-                    <input onChange={changeInput} value={input} placeholder='How can we help you today?'></input>
+                    <input className={css.inpu} onChange={changeInput} value={input} placeholder='How can we help you today?'></input>
                     <button className={css.btn} type='submit'> <i className="fa-solid fa-paper-plane m-2"></i></button>
                 </form>
             </div>
