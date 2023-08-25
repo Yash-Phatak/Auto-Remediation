452ac23dffa516efd1cd39cc1f2c257357bbb1e5
Mitansh Patel <mitanshpatel288@gmail.com>
2023-08-25 12:41:56 +0530

    major frontend upgrade

--- client/src/components/Fp.jsx
+++ client/src/components/Fp.jsx
@@ -11,9 +11,9 @@ function Fp({ open }) {
           <h1 className={fpc.h}>New to the <span className='text-yellow-400'>Crypto</span> World?</h1>
           <h2 style={{ fontFamily: "var(--font2)", fontSize: "1.2rem" }}>Your Trustworthy Partner in Crypto Investment!</h2>
           <div className='flex justify-center items-center'>
-            <div className={fpc.btn} >
+            <Link to='/chat' className={fpc.btn} >
               <Link to='/chat' className='text-yellow-400 text-lg'>Chat now<i className={`fa-solid fa-arrow-right fa-lg ${open? "" : "fa-fade"} pl-3 text-yellow-400 z-0`}></i></Link>
-            </div>
+            </Link>
           </div>
         </div>
         <div className={fpc.img}><img src={img} alt='err' ></img></div>
